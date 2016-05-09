c88ef21d6d5af3e8bb4f29d5ec499e30c5d6e93c
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-05-09 15:00:45 +0300

    fix fetching images in localized images

--- frontend/src/components/common/localizedimage/localizedimageurls.js
+++ frontend/src/components/common/localizedimage/localizedimageurls.js
@@ -1,17 +1,17 @@
 export default {
   fi: {
-    OKM_logo: '../../images/OKM_logo-fi.png',
-    logo: '../../images/logo-fi.png',
-    CSC: '../../images/csc_logo-fi.png'
+    OKM_logo: require('../../../images/OKM_logo-fi.png'),
+    logo: require('../../../images/logo-fi.png'),
+    CSC: require('../../../images/csc_logo-fi.png')
   },
   sv: {
-    OKM_logo: '../../images/OKM_logo-sv.png',
-    logo: '../../images/logo-sv.png',
-    CSC: '../../images/csc_logo-en.png'
+    OKM_logo: require('../../../images/OKM_logo-sv.png'),
+    logo: require('../../../images/logo-sv.png'),
+    CSC: require('../../../images/csc_logo-en.png')
   },
   en: {
-    OKM_logo: '../../images/OKM_logo-en.png',
-    logo: '../../images/logo-en.png',
-    CSC: '../../images/csc_logo-en.png'
+    OKM_logo: require('../../../images/OKM_logo-en.png'),
+    logo: require('../../../images/logo-en.png'),
+    CSC: require('../../../images/csc_logo-en.png')
   }
 }
