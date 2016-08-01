6e2c0566e9a21e956edd39cfebd17c110fac22c7
Jonna Korhonen <jonna.korhonen@csc.fi>
2016-08-01 14:13:25 +0300

    Muokattu tekstejä Kandipalautteen mukaisesti/ Modifications to the text based on Kandipalaute

--- frontend/kandipalaute/src/localizations/localizations_fi.js
+++ frontend/kandipalaute/src/localizations/localizations_fi.js
@@ -1,29 +1,29 @@
 let localizations_fi = {
   header: {
-    logotext: 'Ammattikorkeakoulujen valmistumisvaiheen opiskelijapalaute'
+    logotext: 'Yliopistojen valtakunnallinen opiskelijapalautekysely'
   },
   content: {
     kysely: {
       information: 'Tietoa kyselystä',
-      about1: 'AVOP on Ammattikorkeakoulujen valmistumisvaiheen opiskelijapalautekysely, jossa opiskelija arvioi ja antaa palautetta toteutuneesta koulutuksesta. Kyselyn tuloksia hyödynnetään sekä paikallisesti korkeakoulun oman toiminnan kehittämisessä että valtakunnallisesti koulutuksen ohjauksessa ja rahoituksessa. Kyselyn täyttävät kaikki ammattikorkeakouluista valmistuvat tutkinto-opiskelijat (paitsi Poliisiammattikorkeakoulu).',
-      about2: 'Opiskelijan HAKA-kirjautumisen avulla haetaan henkilön opiskeluoikeuteen liittyviä tietoja VIRTA-opintotietopalvelusta taustatiedoksi AVOP-kyselyn vastaajatunnuksen luomiseksi ja sähköisen palautteen antamisen mahdollistamiseksi. Henkilön yksilöiviä tunnisteita ei välitetä eteenpäin itse AVOP – kyselyyn vaan palautekysely on täysin anonyymi.',
-      about3: 'AVOP -kyselymittaristo on syntynyt Ammattikorkeakoulujen rehtorineuvosto Arene ry:n, Suomen opiskelijakuntien SAMOK ry:n, Opiskelun ja koulutuksen tutkimussäätiön (OTUS) ja opetus- ja kulttuuriministeriön (OKM) yhteisenä hankkeena. Kyselymittariston käyttöönotosta ja sen kehittämisestä vastaa Arene ry.',
+      about1: 'Kandipalaute on yliopistojen valtakunnallinen opiskelijapalautekysely, jossa  selvitetään alemman korkeakoulututkinnon suorittaneiden opiskelijoiden tyytyväisyyttä yliopistoonsa ja kokemuksia opintojen sujumisesta. Kyselyn tuloksia hyödynnetään sekä paikallisesti korkeakoulun oman toiminnan kehittämisessä että valtakunnallisesti koulutuksen ohjauksessa ja rahoituksessa. Kyselyn täyttävät kaikki alempaan korkeakoulututkintoon kuuluvat opinnot suorittaneet yliopisto-opiskelijat.',
+      about2: 'Opiskelijan HAKA-kirjautumisen avulla haetaan henkilön opiskeluoikeuteen liittyviä tietoja VIRTA-opintotietopalvelusta taustatiedoksi Kandipalaute-kyselyn vastaajatunnuksen luomiseksi ja sähköisen palautteen antamisen mahdollistamiseksi. Henkilön yksilöiviä tunnisteita ei välitetä eteenpäin itse Kandipalaute – kyselyyn vaan palautekysely on täysin anonyymi.',
+      about3: 'Kandipalaute -kyselymittaristo on syntynyt Suomen Yliopistot UNIFI ry:n ja opetus- ja kulttuuriministeriön (OKM) yhteisenä hankkeena. Kyselymittariston käyttöönotosta ja sen kehittämisestä vastaa UNIFI ry.',
       privacy: 'Lisätietoja tietosuojaselosteessa.'
     },
     login: {
-      header: 'Ammattikorkeakouluopiskelija',
+      header: 'Yliopisto-opiskelija',
       description: 'Vastaa kyselyyn kirjautumalla oman korkeakoulusi tunnuksilla:'
     },
     tulokset: {
       header: 'Kyselyn tulokset',
-      results1: 'Ammattikorkeakoulujen henkilökunta voi tarkastella kyselyn tuloksia reaaliaikaisesti Arvo - opetushallinnon vaikuttavuustietopalvelun avulla. Katseluoikeudet (Arvo-katselija) voi anoa Opintopolun virkailijantyöpöydän kautta (Omat tiedot)',
+      results1: 'Yliopiston henkilökunta voi tarkastella kyselyn tuloksia reaaliaikaisesti Arvo - opetushallinnon vaikuttavuustietopalvelun avulla. Katseluoikeudet (Arvo-katselija) voi anoa Opintopolun virkailijantyöpöydän kautta (Omat tiedot)',
       results2: 'Kyselyn tuloksista johdettuja tilastoraportteja on avoimesti saatavilla opetushallinnon tilastopalvelu Vipusessa:'
     }
   },
   footer: {
     header: 'Yhteistyössä:',
     arene: {
-      description: 'Ammattikorkeakoulujen rehtorineuvosto Arene ry'
+      description: 'Suomen Yliopistot UNIFI ry'
     },
     okm: {
       description: 'Opetus- ja kulttuuriministeriö'
@@ -36,7 +36,7 @@ let localizations_fi = {
     header: 'Palautekyselyn taustatiedot',
     about: 'Palautekyselyyn välitetään taustatiedoiksi koulutus, ammattikorkeakoulu, koulutuksen kunta, tutkinnon suorituskieli ja koulutusmuoto. Henkilöön liittyviä tietoja ei välitetä palautekyselylle.',
     education: 'Koulutus',
-    school: 'Ammattikorkeakoulu',
+    school: 'Yliopisto',
     municipality: 'Kunta',
     language: 'Tutkinnon suorituskieli',
     form_of_education: 'Koulutusmuoto',
