d30fe30c13a212cf5e55828da8e97541af38364c
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-09-07 08:39:18 +0300

    Virhesivulle header ja footer

--- frontend/avopfi/src/components/Error.js
+++ frontend/avopfi/src/components/Error.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import Translate from 'react-translate-component';
+import Footer from './footer/footer';
+import Header from './header/header';
 
 export default class Error extends React.Component {
   render() {
     var status = this.props.params.status;
     return (
-      <section>
-        <div className="container">
-          <div className="row">
-            <Translate component="h1" content="errors.title"/>
-            {status ===  'haka_error' ?
-              <Translate component="p" content="errors.haka_error"/> :
-              <Translate component="p" content="errors.general_error"/>}
+      <div>
+        <Header></Header>
+
+        <section>
+          <div className="container">
+            <div className="row">
+              <Translate component="h1" content="errors.title"/>
+              {status === 'haka_error' ?
+                <Translate component="p" content="errors.haka_error"/> :
+                <Translate component="p" content="errors.general_error"/>}
+            </div>
           </div>
-        </div>
-      </section>
-    )
+        </section>
+
+        <Footer></Footer>
+      </div>
+  )
   }
 }
-
