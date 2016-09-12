a37929f09b654e4b8325bba615718085552a248c
Lauri Jokipii <lauri.jokipii@csc.fi>
2016-09-12 14:58:03 +0300

    ARVO-141: Toteuta visitors-taulun muutos siten, että tallennettu tieto säilyy.

--- backend/resources/migrations/20160901153000-modify-visitors.down.sql
+++ backend/resources/migrations/20160901153000-modify-visitors.down.sql
@@ -5,7 +5,10 @@ ALTER TABLE visitors ADD COLUMN opiskeluoikeus_id VARCHAR(100);
 ALTER TABLE visitors ADD COLUMN oppilaitos_id VARCHAR(255);
 --;;
 
+UPDATE visitors SET opiskeluoikeus_id=taustatiedot::json->'opiskeluoikeus', oppilaitos_id=taustatiedot::json->'oppilaitos';
+--;;
+
 ALTER TABLE visitors DROP COLUMN taustatiedot;
 --;;
 
-ALTER TABLE visitors RENAME COLUMN vastaajatunnus TO arvo_answers_hash;
+ALTER TABLE visitors RENAME COLUMN vastaajatunnus TO arvo_answer_hash;
--- backend/resources/migrations/20160901153000-modify-visitors.up.sql
+++ backend/resources/migrations/20160901153000-modify-visitors.up.sql
@@ -1,7 +1,14 @@
-ALTER TABLE visitors DROP COLUMN opiskeluoikeus_id;
---;;
-ALTER TABLE visitors DROP COLUMN oppilaitos_id;
---;;
+-- Modify visitors
 ALTER TABLE visitors ADD COLUMN taustatiedot JSON;
 --;;
-ALTER TABLE visitors RENAME COLUMN arvo_answers_hash TO vastaajatunnus;
+
+UPDATE visitors SET taustatiedot=json_build_object('opiskeluoikeus', opiskeluoikeus_id, 'oppilaitos', oppilaitos_id);
+--;;
+
+ALTER TABLE visitors DROP COLUMN opiskeluoikeus_id;
+--;;
+
+ALTER TABLE visitors DROP COLUMN oppilaitos_id;
+--;;
+
+ALTER TABLE visitors RENAME COLUMN arvo_answer_hash TO vastaajatunnus;
