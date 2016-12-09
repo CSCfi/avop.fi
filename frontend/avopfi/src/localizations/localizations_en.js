00ee3d80c7d0c891e0f00778bed7d9ed973be6c4
Jonna Korhonen <jonna.korhonen@csc.fi>
2016-12-09 15:23:45 +0200

    Update localizations_en.js

--- frontend/avopfi/src/localizations/localizations_en.js
+++ frontend/avopfi/src/localizations/localizations_en.js
@@ -29,7 +29,7 @@ let localizations_en = {
       description: 'Ministry of Education and Culture'
     },
     csc: {
-      description: 'Arvo - Education management information service'
+      description: 'Arvo - education management information service'
     }
   },
   profiledata: {
