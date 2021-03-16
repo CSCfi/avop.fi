0d615269d7b4e17bc4c2c2bc2f03533145d9afec
Petri Ryhänen <petri.ryhanen@csc.fi>
2021-03-16 14:05:52 +0200

    CSCARVO-943: Paivita react-scripts versioon 4

--- frontend/kandipalaute/src/index.js
+++ frontend/kandipalaute/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Redirect, Route} from 'react-router-dom';
 import Main from './components/Main';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 let browserLanguage = window.navigator.userLanguage || window.navigator.language;
 if (browserLanguage.indexOf('en') === 0) {
@@ -25,7 +25,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- frontend/rekry/src/components/footer/Footer.js
+++ frontend/rekry/src/components/footer/Footer.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Translate from 'react-translate-component';
 import LocalizedImage from '../common/localizedimage/localizedimage';
 
