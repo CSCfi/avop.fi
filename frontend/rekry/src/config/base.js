cfedc1822abddd7aa6f70558f917ebb91fa5b644
Petri Ryhänen <petri.ryhanen@csc.fi>
2021-03-17 12:31:27 +0200

    CSCARVO-943: Korjaa rekry haka-linkki, typo käännöksessä ja poista NBSC

--- frontend/rekry/src/config/base.js
+++ frontend/rekry/src/config/base.js
@@ -1,6 +1,6 @@
 // Settings configured here will be merged into the final config object.
 const config = {
-  hakaLoginUrl: (currentDomain, lang) => `/rekrykysely/Shibboleth.sso/Login?target=${currentDomain}/rekrykysely/${lang}/user`,
+  hakaLoginUrl: (currentDomain, lang) => `/Shibboleth.sso/Login?target=${currentDomain}/rekrykysely/${lang}/user`,
 }
 
 export default config;
